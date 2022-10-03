4de832bf6d66fe609fd333fa5346efa125eeb1ef
rstrahl <rstrahl@west-wind.com>
2022-10-02 15:34:42 -1000

    Add unversioned files.

---
+++ Westwind.WebStore.Business.Test/AdminBusinessTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Westwind.Webstore.Business;
+using Westwind.Webstore.Business.Test;
+
+namespace Westwind.WebStore.Business.Test;
+
+[TestFixture]
+public class AdminBusinessTests
+{
+    private string ConnectionString = wsApp.Configuration.ConnectionString;
+
+    public BusinessFactory BusinessFactory { get; set; }
+
+    [SetUp]
+    public void Init()
+    {
+        // Load Factory and Provider
+        BusinessFactory = TestHelpers.GetBusinessFactoryWithProvider();
+    }
+
+    [Test]
+    public void SkuEmailListTest()
+    {
+        var adminBus = BusinessFactory.GetAdminBusiness();
+        var list = adminBus.EmailListFromSkus(null, 90);
+
+        Assert.IsNotNull(list);
+        Assert.IsNotEmpty(list);
+
+
+    }
+
+}
