9ef2574baa05febe88379667bbc4c5b4ccb4589f
Rick Strahl <rstrahl@west-wind.com>
2022-12-14 19:02:54 -1000

    Update project references

--- Westwind.Webstore.Web/Program.cs
+++ Westwind.Webstore.Web/Program.cs
@@ -215,8 +215,8 @@ else
     ApiExceptionFilterAttribute.ShowExceptionDetail = config.System.ErrorDisplayMode != ErrorDisplayModes.Application;
 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-
 }
+
 if (wsApp.Configuration.System.RedirectToHttps)
     app.UseHsts();
 
