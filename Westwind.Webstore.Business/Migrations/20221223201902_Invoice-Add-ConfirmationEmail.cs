8bed5fe18e352577946ec2167eb8783c4b27e90b
rstrahl <rstrahl@west-wind.com>
2022-12-23 16:46:46 -0800

    migration files

---
+++ Westwind.Webstore.Business/Migrations/20221223201902_Invoice-Add-ConfirmationEmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Westwind.Webstore.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class InvoiceAddConfirmationEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmationEmail",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmationEmail",
+                table: "Invoices");
+        }
+    }
+}
